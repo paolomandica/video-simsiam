9450e36fbeac3e1ecdb83b82ec117f7a5563450d
paolomandica <paolomandica.p@gmail.com>
2021-12-07 11:54:31 +0000

    Add new TCN aggregator (to fix)

--- code/train.sh
+++ code/train.sh
@@ -16,8 +16,8 @@ python -W ignore train.py --data-path $path_to_kinetics \
 --cache-dataset --cache-path $cache_path \
 --clip-len 8 --workers 40 --lr 0.05 \
 --epochs 100 --batch-size 40 \
---output-dir "./checkpoints/simsiam_lstm/" \
---visualize --data-parallel
+--output-dir "./checkpoints/simsiam_tcn/" \
+--visualize --data-parallel --name "tcn"
 # --port 8094 
 # --partial-reload "../pretrained.pth"
 # --resume "./checkpoints/randomise_sp_unnorm/checkpoint.pth"
